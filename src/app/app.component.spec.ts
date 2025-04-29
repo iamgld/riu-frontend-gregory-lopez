03aa84572b98b484ceed6aedaf858bcb925699a9
iamgld <contact@iamgld.dev>
2025-04-29 19:25:34 -0300

    test: refactor AppComponent tests to use 'test' instead of 'it' and new test cases were added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,10 @@
 // Angular Imports
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideRouter } from '@angular/router'
 // This Component Imports
 import { AppComponent } from './app.component'
+// Shared Imports
+import { MaterialModule } from '@shared/modules'
 
 describe('AppComponent', () => {
 	let component: AppComponent
@@ -9,7 +12,8 @@ describe('AppComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [AppComponent],
+			imports: [MaterialModule],
+			providers: [provideRouter([])],
 		}).compileComponents()
 
 		fixture = TestBed.createComponent(AppComponent)
@@ -17,7 +21,26 @@ describe('AppComponent', () => {
 		fixture.detectChanges()
 	})
 
-	it('should create', () => {
+	test('should be create', () => {
 		expect(component).toBeTruthy()
+	})
+
+	test('should render the title "Heroes" in the toolbar', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		const title = compiled.querySelector('.toolbar__section__title')
+		expect(title?.textContent).toContain('Heroes')
+	})
+
+	test('should render the LoaderComponent', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		const loader = compiled.querySelector('app-loader')
+		expect(loader).toBeTruthy()
+	})
+
+	test('should render the router-outlet inside LoaderComponent', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		const loader = compiled.querySelector('app-loader')
+		const routerOutlet = loader?.querySelector('router-outlet')
+		expect(routerOutlet).toBeTruthy()
 	})
 })
